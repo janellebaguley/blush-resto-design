b03d1795edcf64e76555350a490c0df0ddacb563
janellebaguley <nellie.baguley@gmail.com>
2021-01-28 16:06:27 -0700

    updates

--- server/index.js
+++ server/index.js
@@ -4,9 +4,17 @@ const express = require('express'),
       productCtrl = require('./controllers/productController'),
       massive = require('massive'),
       session = require('express-session')
-const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env;
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET, S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY} = process.env;
 
 const app = express();
+const aws = require('aws-sdk');
+
+// app.get('/api/signs3', (req, res) => {
+//   aws.config = {
+//     region: 'us-west-1',
+//     accessKeyId: AWS_ACCESS_KEY_ID,
+//     secretAccessKey: AWS_SECRET_ACCESS_KEY,
+//   };
 
 app.use(express.json());
 app.use(session({
@@ -24,6 +32,30 @@ massive({
     app.set('db', db)
     console.log('db connected')
 })
+// const s3 = new aws.S3();
+//   const fileName = req.query['file-name'];
+//   const fileType = req.query['file-type'];
+//   const s3Params = {
+//     Bucket: S3_BUCKET,
+//     Key: fileName,
+//     Expires: 60,
+//     ContentType: fileType,
+//     ACL: 'public-read',
+//   };
+
+//   s3.getSignedUrl('putObject', s3Params, (err, data) => {
+//     if (err) {
+//       console.log(err);
+//       return res.end();
+//     }
+//     const returnData = {
+//       signedRequest: data,
+//       url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`,
+//     };
+
+//     return res.send(returnData);
+//   });
+// });
 
 //Auth endpoints
 app.post('/api/register', authCtrl.register)
