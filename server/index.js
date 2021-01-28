754aa357994ba6f01af8e880ac24d5625f155dbf
janellebaguley <nellie.baguley@gmail.com>
2021-01-28 16:30:52 -0700

    updated

--- server/index.js
+++ server/index.js
@@ -9,12 +9,36 @@ const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET, S3_BUCKET, AWS_ACCESS_KEY
 const app = express();
 const aws = require('aws-sdk');
 
-// app.get('/api/signs3', (req, res) => {
-//   aws.config = {
-//     region: 'us-west-1',
-//     accessKeyId: AWS_ACCESS_KEY_ID,
-//     secretAccessKey: AWS_SECRET_ACCESS_KEY,
-//   };
+app.get('/api/signs3', (req, res) => {
+  aws.config = {
+    region: 'us-west-1',
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  };
+const s3 = new aws.S3();
+  const fileName = req.query['file-name'];
+  const fileType = req.query['file-type'];
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+    Expires: 60,
+    ContentType: fileType,
+    ACL: 'public-read',
+  };
+
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.end();
+    }
+    const returnData = {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`,
+    };
+
+    return res.send(returnData);
+  });
+});
 
 app.use(express.json());
 app.use(session({
@@ -32,30 +56,7 @@ massive({
     app.set('db', db)
     console.log('db connected')
 })
-// const s3 = new aws.S3();
-//   const fileName = req.query['file-name'];
-//   const fileType = req.query['file-type'];
-//   const s3Params = {
-//     Bucket: S3_BUCKET,
-//     Key: fileName,
-//     Expires: 60,
-//     ContentType: fileType,
-//     ACL: 'public-read',
-//   };
 
-//   s3.getSignedUrl('putObject', s3Params, (err, data) => {
-//     if (err) {
-//       console.log(err);
-//       return res.end();
-//     }
-//     const returnData = {
-//       signedRequest: data,
-//       url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`,
-//     };
-
-//     return res.send(returnData);
-//   });
-// });
 
 //Auth endpoints
 app.post('/api/register', authCtrl.register)
