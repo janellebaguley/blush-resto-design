d214d9505560e6758fc5fc1cb2faedb1f9f5bb29
janellebaguley <nellie.baguley@gmail.com>
2021-02-05 11:09:58 -0700

    checkout update

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,62 +1,20 @@
 import React, {Component} from 'react';
-// import {connect} from 'react-redux'
-// import {updateCart, clearCart, getCart} from '../../redux/cartReducer'
 import axios from 'axios';
-import UpdateCart from './UpdateCart'
-import './Checkout.css'
 
 class Checkout extends Component {
-        constructor(){
-            super()
-            this.state = {
-            user: {},
-            orderItems: [],
-            furniture: [],
-            total: 0.00
-            }
-        }
-
-    componentDidMount(){
-            this.handleGetUser()
-    }
-
-    handleGetUserCart = async() => {
-        await axios.get (`/api/cart/${this.state.user.user_id}`)
+    handleCheckout = () => {
+        axios.post('/api', {amount: Math.round(this.props.total), order_id: this.props.orders, user_id: this.props.user, date: today})
         .then(res => {
-            this. setState({orderItems: res.data})
+            this.props.getUser()
         })
-        let sum = this.state.orderItems.reduce((acc, curr) => {
-            return acc + parseFloat(curr.price)
-        }, 0)
-        this.setState({total: Math.round(sum * 100) / 100})
     }
-    
-    
-    
-        render(){
-            const mappedCart= this.state.orderItems.map((item, i) => {
-                return (
-                    <UpdateCart
-                    key = {i}
-                    cart= {item}
-                    getCart= {this.handleGetUserCart}/>
-                )
-            })
+    render () {
         return(
-            <div className='checkout'>
-                <div>Cart</div>
-                <section>
-                   <h6>Total: ${this.state.total}</h6>
-                </section>
-                    
-                </div>       
+            <div>
+                <h1>Checkout</h1>
+            </div>
         )
     }
 }
-// const mapStateToProps = (reduxState) => {
-//     return {
-//         ...reduxState.cart,
-//         ...reduxState.user
-//     }
-// }
+
 export default Checkout;
